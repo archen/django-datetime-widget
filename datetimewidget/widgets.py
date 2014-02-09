d1804645409a1b6cb9445880518a2e65efc09e0b
Kevin Glavin <archen.sol@gmail.com>
2014-02-09 17:27:30 -0500

    updated to use new bootstrap-datetimepicker and for Python 3.x compatibility

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -22,20 +22,6 @@ $.fn.datetimepicker.dates['en'] = {
 
 datetimepicker_options = """
     format : '%s',
-    startDate : '%s',
-    endDate : '%s',
-    weekStart : %s,
-    daysOfWeekDisabled : %s,
-    autoclose : %s,
-    startView : %s,
-    minView : %s,
-    maxView : %s,
-    todayBtn : %s,
-    todayHighlight : %s,
-    minuteStep : %s,
-    pickerPosition : '%s',
-    showMeridian : %s,
-    clearBtn : %s,
     language : '%s',
 """
 
@@ -97,21 +83,6 @@ class DateTimeWidget(MultiWidget):
             self.option += (options.get('format','dd/mm/yyyy hh:ii'),)
             self.format = pattern.sub(lambda x: dateConversiontoPython[x.group()], self.option[0])
 
-        self.option += (options.get('startDate',''),)
-        self.option += (options.get('endDate',''),)
-        self.option += (options.get('weekStart','0'),)
-        self.option += (options.get('daysOfWeekDisabled','[]'),)
-        self.option += (options.get('autoclose','true'),)
-        self.option += (options.get('startView','2'),)
-        self.option += (options.get('minView','0'),)
-        self.option += (options.get('maxView','4'),)
-        self.option += (options.get('todayBtn','false'),)
-        self.option += (options.get('todayHighlight','false'),)
-        self.option += (options.get('minuteStep','5'),)
-        self.option += (options.get('pickerPosition','bottom-right'),)
-        self.option += (options.get('showMeridian','false'),)
-        self.option += (options.get('clearBtn','true'),)
-
         self.language = options.get('language', 'en')
         self.option += (self.language,)
 
@@ -164,7 +135,7 @@ class DateTimeWidget(MultiWidget):
         id = uuid.uuid4().hex
         return '<div id="%s"  class="input-append date form_datetime">'\
                '%s'\
-               '<span class="add-on"><i class="icon-th"></i></span>'\
+               '<span class="add-on"><i data-time-icon="icon-time" data-date-icon="icon-calendar"></i></span>'\
                '</div>'\
                '<script type="text/javascript">'\
                '$("#%s").datetimepicker({%s});'\
