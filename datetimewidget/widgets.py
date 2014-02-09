bb8e31efa697f92dff93b26fde4efe73c2b355ab
Kevin Glavin <archen.sol@gmail.com>
2014-02-09 17:36:29 -0500

    icon tinkering

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -135,7 +135,7 @@ class DateTimeWidget(MultiWidget):
         id = uuid.uuid4().hex
         return '<div id="%s"  class="input-append date form_datetime">'\
                '%s'\
-               '<span class="add-on"><i data-time-icon="icon-time" data-date-icon="icon-calendar"></i></span>'\
+               '<span class="add-on"><i data-time-icon="glyphicon-time" data-date-icon="glyphicon-calendar"></i></span>'\
                '</div>'\
                '<script type="text/javascript">'\
                '$("#%s").datetimepicker({%s});'\
